c06bf0c50b4e81f3646f3059c0998a3caf0d8b0a
Julia Gabrielle <107364740+juliagbmaciel@users.noreply.github.com>
2023-06-23 17:16:30 +0000

    update objetos <3

--- extras-23-06/objetos.js
+++ extras-23-06/objetos.js
@@ -11,31 +11,73 @@ console.log(pessoa['nome'])
 console.log(pessoa.nome)
 
 //Maneira de criar objeto com construtor
-const objeto = new Objeto('linda')
-function Objeto(beleza){
-    this.nome = 'julia'
-    this.sobrenome = 'gabrielle maciel'
+function Objeto(variavel, variavel2){
+    this.nome = 'julia';
+    this.sobrenome = 'gabrielle maciel';
     //maneira de criar metodo dentro de objeto com construtor
-    this.fala = function(){
-        //se eu nao utilizo return, ele me retorna um valor "undefined"
-        return `${this.nome} esta falando..`
-    }
+    // this.fala = function(){
+    //     //se eu nao utilizo return, ele me retorna um valor "undefined"
+    //     return `${this.nome} esta falando..`
+    // }
     //property que declara se vai permitir ou nao que o usuario configure aquele atributo
-    Object.defineProperty(this, beleza, {
+    //nao esquecer de mandar como string....
+    Object.defineProperty(this, 'variavel', {
         enumerable: true, //define se vai aparecer na tela
-        value: beleza, //define qual o valor ele vai ter
+        value: variavel, //define qual o valor ele vai ter
         writable: true, //define se pode sobescrever
-        configurable: false, //define se pode configurar, exemplo deletar
-    })
+        configurable: true //define se pode configurar, exemplo deletar
+    });
 }
-console.log(objeto.nome)
+const objeto = new Objeto('oi', 'tdbem')
+
 
 //ou sem a função, podemos criar os atributos diretamente:
-objeto.idade = 19
-console.log(objeto.fala())
+//objeto.idade = 19
+
 
 //caso eu queira travar o meu objeto, ou seja, não permitir que seja alterado nada dentro dele:
-Object.freeze(objeto)
-objeto.nome = 'kethelyn santos'
+//Object.freeze(objeto)
 
-console.log(objeto)
+//ver chaves do objeto 
+console.log(Object.keys(objeto))
+
+
+
+//criando objeto com defineProperties
+function Tvs(marca, valor, cor){
+    let valorTvs = valor;
+    Object.defineProperties(this, {
+        //julia é a chave que sera atribuida, posso colocar literalmente qqr coisa que vai virar o atributo da minha classe
+        julia:{
+            enumerable: true,
+            //value é o valor que esta vindo de marca como parametro
+            value: marca,
+            writable: false,
+            configurable: false
+        },
+        valor: {
+            enumerable: false,
+            configurable: false,
+            //getter para pegar o valor das tvs
+            get: function(){
+                return valorTvs;
+            },
+            set: function(teste){
+                if (typeof teste != 'number'){
+                    console.log('digite um numero')
+                }
+            }
+        },
+        cor: {
+            enumerable: true,
+            value: cor,
+            writable: false,
+            configurable: false
+        }
+    });
+}
+
+
+const instanciaTvs = new Tvs('LG', 1900, 'preto')
+instanciaTvs.valor = 'julinha'
+console.log(instanciaTvs.julia)
